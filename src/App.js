b3ea96f16d60c87e9c3564862a10da908335b80f
lshaikh <lshaikh@xento.com>
2021-11-25 14:49:31 +0530

    one change is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -26,17 +26,12 @@ const App = () => {
     },
   ];
 
+  const [expenses, setExpenses] = useState( DUMMY_EXPENSES );
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+      setExpenses((prevExpenses) => {
+       return [expense, ...prevExpenses];
+      });
+  }; 
 
   return (
     <div>
